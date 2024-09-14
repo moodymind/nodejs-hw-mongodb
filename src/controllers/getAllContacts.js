5e12c8faa6f8486de6eb13dabfc5f596142dad7c
Austin Miller <vadim.sukhetskii@gmail.com>
2024-09-14 15:49:11 +0200

    fixes for not correctly searching the contacts

---
+++ src/controllers/getAllContacts.js
@@ -0,0 +1,16 @@
+import { getAllContacts } from '../services/contacts.js';
+
+export const getContacts = async (req, res) => {
+  try {
+    const contacts = getAllContacts();
+    res.status(200).json({
+      message: 'Successfully found contacts',
+      data: contacts,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: 'Failed to find contacts',
+      error: e.message,
+    });
+  }
+};
---
+++ src/controllers/getContactById.js
@@ -0,0 +1,17 @@
+import { getContactById } from '../services/contacts.js';
+
+export const getContact = async (req, res) => {
+  try {
+    const contact = await getContactById(req.params.contactId);
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${req.params.contactId}!`,
+      data: contact,
+    });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
