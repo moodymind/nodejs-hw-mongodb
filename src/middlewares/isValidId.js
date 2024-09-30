f457d637a48823c1fce173003ae12732e5622cbe
Austin Miller <vadim.sukhetskii@gmail.com>
2024-09-30 17:46:16 +0200

    fixed 400 bad request while searching by ID

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { studentId } = req.params;
+  const { contactId } = req.params;
 
-  if (!isValidObjectId(studentId)) {
+  if (!isValidObjectId(contactId)) {
     throw createHttpError(400, 'Bad request');
   }
 
